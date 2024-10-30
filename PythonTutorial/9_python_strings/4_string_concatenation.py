effc5f1ad91fdbc05969923ac078ea2af9f30886
Naser Sami <naser_ebedo@icloud.com>
2024-10-30 22:19:33 +0300

    PW3 - String Concatenation

---
+++ PythonTutorial/9_python_strings/4_string_concatenation.py
@@ -0,0 +1,37 @@
+
+# Python - String Concatenation
+
+# String Concatenation
+# To concatenate, or combine, two strings you can use the + operator.
+
+# Merge variable a with variable b into variable c:
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+
+# To add a space between them, add a " ":
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
+# Exercise
+# What is a correct syntax to merge variable x and y into variable z?
+# z = x + y
+
+
+# What will be the result of the following code:
+x = 'Welcome'
+y = 'Coders'
+print(x + y)    # WelcomeCoders
+
+
+# Consider this code:
+a = 'Join'
+b = 'the'
+c = 'party'
+# What is a correct syntax to print 'Join the party'?
+print(a + ' ' + b + ' ' + c)
