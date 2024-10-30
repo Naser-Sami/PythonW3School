9a8ffc9eae59cb7b89cbcf209053f72f41d7f650
Naser Sami <naser_ebedo@icloud.com>
2024-10-30 15:35:24 +0300

    PW3

---
+++ PythonTutorial/3_python_syntax/syntax.py
@@ -0,0 +1,29 @@
+# Python Indentation
+
+# Python uses indentation to indicate a block of code.
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+# Python will give you an error if you skip the indentation:
+
+# if 5 > 2:
+# print("Five is greater than two!")
+
+
+# You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
+
+# if 5 > 2:
+#  print("Five is greater than two!")
+#         print("Five is greater than two!")
+
+
+# --------------------
+
+# Python Variables
+x = 5
+y = "Hello, World!"
+
+# Comments
+# This is a comment.
+print("Hello, World!")
