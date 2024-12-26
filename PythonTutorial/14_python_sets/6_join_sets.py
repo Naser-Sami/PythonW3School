d2ca32390596c2a106435c64262017849b1abc51
Naser Sami <naser_ebedo@icloud.com>
2024-12-26 11:27:02 +0300

    P.W3

--- PythonTutorial/14_python_sets/6_join_sets.py
+++ PythonTutorial/14_python_sets/6_join_sets.py
@@ -113,3 +113,62 @@ set2 = {False, "google", 1, "apple", 2, True}
 set3 = set1.intersection(set2)
 print(set3)
 
+
+print('\nDifference:\n')
+# Difference
+# The difference() method will return a new set
+# that will contain only the items from the first set that are not present in the other set.
+
+# Keep all items from set1 that are not in set2:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.difference(set2)
+print(set3)
+
+# ou can use the - operator instead of the difference() method, and you will get the same result.
+# Use - to join two sets:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 - set2
+print(set3)
+# Note: The - operator only allows you to join sets with sets,
+# and not with other data types like you can with the difference() method.
+
+# The difference_update() method will also keep the items from the first set that are not in the other set,
+# but it will change the original set instead of returning a new set.
+
+# Use the difference_update() method to keep the items that are not present in both sets:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.difference_update(set2)
+print(set1)
+
+
+# Symmetric Differences
+# The symmetric_difference() method will keep only the elements that are NOT present in both sets.
+
+print('\nSymmetric Differences:\n')
+
+# Keep the items that are not present in both sets:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1.symmetric_difference(set2)
+print(set3)
+
+# You can use the ^ operator instead of the symmetric_difference() method, and you will get the same result.
+set3 = set1 ^ set2
+print(set3)
+
+# Note: The ^ operator only allows you to join sets with sets,
+# and not with other data types like you can with the symmetric_difference() method.
+
+# The symmetric_difference_update() method will also keep all but the duplicates,
+# but it will change the original set instead of returning a new set.
+
+# Use the symmetric_difference_update() method to keep the items that are not present in both sets:
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.symmetric_difference_update(set2)
+print(set1)
