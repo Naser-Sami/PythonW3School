fa20a9984fd7d970eff5952c1ded6ed211f96a37
Naser Sami <naser_ebedo@icloud.com>
2025-01-07 23:58:16 +0300

    P.W3

---
+++ Matplotlib/1_installation_of_matplotlib.py
@@ -0,0 +1,12 @@
+
+# Installation of Matplotlib
+
+# pip install matplotlib
+
+# Checking Matplotlib Version
+# The version string is stored under __version__ attribute.
+
+
+import matplotlib
+
+print(matplotlib.__version__)
